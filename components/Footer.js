c53c5e9660da7d354709718e2e292c2029d1f1ed
tuyen2tech <164856147+tuyen2tech@users.noreply.github.com>
2024-03-25 22:40:26 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-      <p>Chính sách bảo mật | Điều khoản sử dụng  </p>
+      <p>Chính sách bảo mật | Điều khoản sử dụng  </p> </br>
 <p>Copyright © 2024 79CLUB INC.  </p>
   
       </footer>
