1170f2068695f8509817a2f03b46d74ee77ef410
tuyen2tech <164856147+tuyen2tech@users.noreply.github.com>
2024-03-25 16:47:47 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="79CLUB" />
     <p>HƯỚNG DẪN CÀI ĐẶT</p>
-<div class="scroll-viewer"><picture><img alt="" src="https://vn78.netlify.app/static/media/step-webclip-vi.6ed5bd73.png" style="height: 260px; max-width: unset;"></picture></div>
+<div ><picture><img alt="" src="https://vn78.netlify.app/static/media/step-webclip-vi.6ed5bd73.png" style="height: 260px; max-width: unset;"></picture></div>
 <p className="description">Tham gia 79club cùng với hàng triệu người chơi từ mọi nơi trên thế giới! Đắm mình vào thế giới sôi động, đặt cược và chiến thắng để chứng minh rằng bạn là người chiến thắng thực sự. Đầu tư và nâng cao cược của bạn, cải thiện kỹ năng, tích lũy kinh nghiệm và trở thành người chơi giỏi nhất từ trước đến nay!</p>
     
         <p className="description">
