9c14fd542053c3007c3341c3bc6c6d2e661b0f0d
tuyen2tech <164856147+tuyen2tech@users.noreply.github.com>
2024-03-25 21:40:58 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -38,18 +38,7 @@ export default function Home() {
 
 <p className="description">Tham gia 79club cùng với hàng triệu người chơi từ mọi nơi trên thế giới! Đắm mình vào thế giới sôi động, đặt cược và chiến thắng để chứng minh rằng bạn là người chiến thắng thực sự. Đầu tư và nâng cao cược của bạn, cải thiện kỹ năng, tích lũy kinh nghiệm và trở thành người chơi giỏi nhất từ trước đến nay!</p>
 <p>Có gì mới?</p>  
-    <p>Version 1.0.0 
-    * ONE ACCOUNT – Bắt đầu chơi trên điện thoại thông minh hoặc máy tính bảng của bạn và tiếp tục chơi ở mọi nơi mà không làm mất tiến trình của bạn.</p>
-    
-        
-    <p>
-Quyền riêng tư:
-    Nhiệm vụ của 79CLUB là kết nối thế giới thông qua các trò chơi. Chúng tôi mang đến những trải nghiệm giải trí xã hội, thú vị cho mọi người trên khắp thế giới. Để theo đuổi sứ mệnh của mình, chúng tôi thu thập và sử dụng một số thông tin nhất định từ người chơi của mình. Minh bạch về việc chúng tôi sử dụng thông tin của người chơi và tôn trọng quyền riêng tư của họ là vô cùng quan trọng đối với 79CLUB.
-
-Chính sách Bảo mật này áp dụng khi bạn sử dụng Dịch vụ của chúng tôi (được mô tả bên dưới). Vui lòng đọc trước khi sử dụng Dịch vụ của 79CLUB vì nó sẽ cho bạn biết cách chúng tôi hu thập, lưu trữ, sử dụng, tiết lộ và xử lý thông tin cá nhân của bạn khi cung cấp các Dịch vụ này cho bạn. Tuy nhiên, Chính sách Bảo mật này không phải là một hợp đồng và không tạo ra bất kỳ quyền hoặc nghĩa vụ pháp lý nào.
-
-Nếu bạn có bất kỳ câu hỏi, nhận xét hoặc thắc mắc nào liên quan đến Chính sách Bảo mật hoặc các thông lệ của chúng tôi, vui lòng liên hệ với chúng tôi.
-    </p>
+   
  
       </main>
 
