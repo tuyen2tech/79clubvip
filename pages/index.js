560880574524c8279d16cc88c61c8c7df6100848
tuyen2tech <164856147+tuyen2tech@users.noreply.github.com>
2024-03-25 21:29:56 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
     <table> 
     <tr>
     <td>
-    <p> <img src="https://anhafood.com/logo.jpg" width="120px" />
+    <p> <img src="https://anhafood.com/logo.jpg" width="135px" />
     </p>
     </td>
      <td>
