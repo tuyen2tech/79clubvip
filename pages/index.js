377d11acee4e9507a1e3fad7637c5858e830b4f5
tuyen2tech <164856147+tuyen2tech@users.noreply.github.com>
2024-03-25 20:27:23 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,21 @@ export default function Home() {
 
       <main>
         <Header title="79CLUB" />
+    <table> 
+    <tr>
+    <td>Logo</td>
+     <td>
+    <p>79CLUB</p>
+    <p>Nhà cái hàng đầu châu Á</p>
+    <p>Tải xuống miễn phí:</p>
+    </td>
+    </tr>
+    <tr>
+    <td><button name="button" type="button">iOS</button></td>
+    <td><button name="button" type="button">Android</button></td>
+    </tr>
+    
+    </table>
     <p className="description">HƯỚNG DẪN CÀI ĐẶT</p>
 
 <p className="description">Tham gia 79club cùng với hàng triệu người chơi từ mọi nơi trên thế giới! Đắm mình vào thế giới sôi động, đặt cược và chiến thắng để chứng minh rằng bạn là người chiến thắng thực sự. Đầu tư và nâng cao cược của bạn, cải thiện kỹ năng, tích lũy kinh nghiệm và trở thành người chơi giỏi nhất từ trước đến nay!</p>
